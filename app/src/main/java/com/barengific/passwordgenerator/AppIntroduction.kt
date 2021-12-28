cfc65fae5a2d4313b92c2b3402e3ade632a6cc3f
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-28 20:14:14 +0000

    App Introduction
    Added an introduction to the application.

--- app/src/main/java/com/barengific/passwordgenerator/AppIntroduction.kt
+++ app/src/main/java/com/barengific/passwordgenerator/AppIntroduction.kt
@@ -8,14 +8,38 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.github.appintro.AppIntro
+import com.github.appintro.AppIntroFragment
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
-class AppIntroduction : AppCompatActivity() {
+class AppIntroduction : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Make sure you don't call setContentView!
 
+        // Call addSlide passing your Fragments.
+        // You can use AppIntroFragment to use a pre-built fragment
+        addSlide(
+            AppIntroFragment.newInstance(
+            title = "Welcome...baz",
+            description = "This is MYY the first slide of the example"
+        ))
+        addSlide(AppIntroFragment.newInstance(
+            title = "Yehh...Let's get started!",
+            description = "This is the last slide, I won't annoy you more :)"
+        ))
     }
 
-}
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        // Decide what to do when the user clicks on "Skip"
+        finish()
+    }
 
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        // Decide what to do when the user clicks on "Done"
+        finish()
+    }
+}

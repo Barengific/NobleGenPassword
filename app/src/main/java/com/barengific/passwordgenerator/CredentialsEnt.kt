0941bffcd7d4b441590b3146835cf92c246bf760
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-29 11:26:34 +0000

    Update CredentialsEnt.kt
    Cleanup

--- app/src/main/java/com/barengific/passwordgenerator/CredentialsEnt.kt
+++ app/src/main/java/com/barengific/passwordgenerator/CredentialsEnt.kt
@@ -24,9 +24,8 @@ import kotlinx.android.synthetic.main.credentials_ent.*
 import java.io.File
 
 
-class CredentialsEnt : AppCompatActivity(){
+class CredentialsEnt : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,58 +70,59 @@ class CredentialsEnt : AppCompatActivity(){
                         hidePasswordMethod //hides password
             }
         }
-        btnSubmit.setOnClickListener{
-            if(editTextTextPassword.text.toString().isNotEmpty()
+        btnSubmit.setOnClickListener {
+            if (editTextTextPassword.text.toString().isNotEmpty()
                 && editTextNumberPassword1.text.toString().isNotEmpty()
                 && editTextNumberPassword2.text.toString().isNotEmpty()
                 && editTextNumberPassword3.text.toString().isNotEmpty()
-                && editTextNumberPassword4.text.toString().isNotEmpty()){
-                if(editTextTextPassword.text.toString().length >= 8){
-                    if(editTextNumberPassword1.text.toString().length == 1
+                && editTextNumberPassword4.text.toString().isNotEmpty()
+            ) {
+                if (editTextTextPassword.text.toString().length >= 8) {
+                    if (editTextNumberPassword1.text.toString().length == 1
                         && editTextNumberPassword2.text.toString().length == 1
                         && editTextNumberPassword3.text.toString().length == 1
-                        && editTextNumberPassword4.text.toString().length == 1){
-                        editTextTextPassword.visibility = View.INVISIBLE
-                        editTextNumberPassword1.visibility = View.INVISIBLE
-                        editTextNumberPassword2.visibility = View.INVISIBLE
-                        editTextNumberPassword3.visibility = View.INVISIBLE
-                        editTextNumberPassword4.visibility = View.INVISIBLE
-                        switch1.visibility = View.INVISIBLE
-                        btnSubmit.visibility = View.INVISIBLE
-
+                        && editTextNumberPassword4.text.toString().length == 1
+                    ) {
                         //val path = applicationContext.getFilesDir()
                         //Log.d("aaa", path.toString())
 
                         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                        Log.d("aaa",
-                            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                                .toString()
-                        )
-//                            val PERMISSION_ALL = 1
-//                            val PERMISSIONS = arrayOf(
-//                                Manifest.permission.READ_EXTERNAL_STORAGE,
-//                                Manifest.permission.WRITE_EXTERNAL_STORAGE
-//                            )
-                        File(
-                            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                            .toString()+"/barzzzz.txt").writeText("hello,hello,heloo barzz")
+                        Log.d("aaa", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString())
+
+                        File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/barz.txt").writeText(
+                            editTextTextPassword.text.toString() +"\n" +
+                                    editTextNumberPassword1.text.toString() +"\n" +
+                                    editTextNumberPassword2.text.toString() +"\n" +
+                                    editTextNumberPassword3.text.toString() +"\n" +
+                                    editTextNumberPassword4.text.toString())
 
                         val intent = Intent(this, MainActivity::class.java).apply {
-//            putExtra(EXTRA_MESSAGE, message)
+                            //putExtra(EXTRA_MESSAGE, message)
                         }
                         Log.d("aaa", "in credentials")
                         startActivity(intent)
-                    }else{
-                        Toast.makeText(applicationContext, "Sigma values cannot be more than 1 digit", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Sigma values cannot be more than 1 digit",
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
-                }else{
-                    Toast.makeText(applicationContext, "Master Key has to be above 8 characters", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Master Key has to be above 8 characters",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
-            } else{
+            } else {
                 Log.d("aaa", "buttonsss")
-                Toast.makeText(applicationContext, "Please fill in all the required fields", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Please fill in all the required fields",
+                    Toast.LENGTH_LONG
+                ).show()
             }
-
 
         }
 

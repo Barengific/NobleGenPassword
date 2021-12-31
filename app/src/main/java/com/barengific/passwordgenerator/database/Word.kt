593b7ca01dd6bc76f28f894e2b15405a6798391f
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-31 20:13:07 +0000

    Update Word.kt
    Auto gen id, in the entry creation type 0

--- app/src/main/java/com/barengific/passwordgenerator/database/Word.kt
+++ app/src/main/java/com/barengific/passwordgenerator/database/Word.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Word(
-    @PrimaryKey val wid: Int,
+    @PrimaryKey(autoGenerate = true) val wid: Int = 0,
     @ColumnInfo(name = "p_type") val pType: String?,
     @ColumnInfo(name = "keyer") val key: String?,
     @ColumnInfo(name = "valuer") val value: String?

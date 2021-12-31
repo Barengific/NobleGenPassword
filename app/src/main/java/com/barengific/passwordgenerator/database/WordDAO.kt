7c779086a5ddb80715aee8a0440f2ca997c74962
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-31 18:40:17 +0000

    Update WordDAO.kt

--- app/src/main/java/com/barengific/passwordgenerator/database/WordDAO.kt
+++ app/src/main/java/com/barengific/passwordgenerator/database/WordDAO.kt
@@ -1,4 +1,30 @@
 package com.barengific.passwordgenerator.database
 
-class WordDAO {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+    @Query("SELECT * FROM word")
+    fun getAll(): List<Word>
+
+    @Query("SELECT * FROM word WHERE wid IN (:wordIds)")
+    fun loadAllByIds(wordIds: IntArray): List<Word>
+
+    @Query("SELECT * FROM word WHERE keyer LIKE :k")
+    fun findByKey(k: String): Word
+
+    @Query("SELECT * FROM word WHERE valuer LIKE :v")
+    fun findByVal(v: String): Word
+
+    @Query("SELECT * FROM word WHERE p_type LIKE :pType")
+    fun findByType(pType: String): Word
+
+    @Insert
+    fun insertAll(vararg users: Word)
+
+    @Delete
+    fun delete(word: Word)
 }

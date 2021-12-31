9bc15f47a968fca943ba12c9986c46b881f46c39
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-31 20:12:38 +0000

    Update AppDatabase.kt
    Allow db query to be made with main thread

--- app/src/main/java/com/barengific/passwordgenerator/database/AppDatabase.kt
+++ app/src/main/java/com/barengific/passwordgenerator/database/AppDatabase.kt
@@ -1,4 +1,27 @@
 package com.barengific.passwordgenerator.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        private lateinit var INSTANCE:AppDatabase
+        private lateinit var context: Context
+        fun getInstance(con: Context):AppDatabase= Room.databaseBuilder(
+            con,
+            AppDatabase::class.java,
+            "word"
+        )
+
+            .createFromAsset("word.db")
+            .allowMainThreadQueries()
+            .build()
+
+    }
+
+    abstract fun wordDao(): WordDao
+
 }

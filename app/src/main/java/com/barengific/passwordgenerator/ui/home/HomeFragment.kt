b90f115951d2110021bb8a8bbeecdfb0fcbb25fa
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-01 14:37:11 +0000

    Update HomeFragment.kt

--- app/src/main/java/com/barengific/passwordgenerator/ui/home/HomeFragment.kt
+++ app/src/main/java/com/barengific/passwordgenerator/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.barengific.passwordgenerator.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
@@ -13,6 +15,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.barengific.passwordgenerator.CredentialsEnt
+import com.barengific.passwordgenerator.MainActivity
 import com.barengific.passwordgenerator.R
 import com.barengific.passwordgenerator.databinding.FragmentHomeBinding
 
@@ -25,6 +29,8 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var here = 0;
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,6 +41,16 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        if(here == 0){
+            here += 1
+        }else if(here == 1){
+            val intent = Intent(getActivity(), MainActivity::class.java).apply {
+//            putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
+        }
+
 
 
         return root

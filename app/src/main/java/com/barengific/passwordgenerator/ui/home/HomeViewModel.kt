b19dd99b533ff1a29e34b17aeb75bbb0f5ab9bd5
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-01 14:37:03 +0000

    Update HomeViewModel.kt

--- app/src/main/java/com/barengific/passwordgenerator/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/barengific/passwordgenerator/ui/home/HomeViewModel.kt
@@ -1,13 +1,16 @@
 package com.barengific.passwordgenerator.ui.home
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.barengific.passwordgenerator.AppIntroduction
+import com.barengific.passwordgenerator.CredentialsEnt
+import com.barengific.passwordgenerator.MainActivity
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+
 }

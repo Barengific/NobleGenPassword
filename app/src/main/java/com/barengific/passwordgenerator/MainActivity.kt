3484c85fc3031c348f9105bd5a63d67fc32221ec
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-14 19:47:07 +0000

    Update MainActivity.kt

--- app/src/main/java/com/barengific/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/barengific/passwordgenerator/MainActivity.kt
@@ -145,7 +145,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 //

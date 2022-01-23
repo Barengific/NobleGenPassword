99fd83aee972338c2e81b66deb93e3887d7db63b
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-23 19:27:37 +0000

    Update Backup.kt

--- app/src/main/java/com/barengific/passwordgenerator/Backup.kt
+++ app/src/main/java/com/barengific/passwordgenerator/Backup.kt
@@ -1,13 +1,38 @@
 package com.barengific.passwordgenerator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.barengific.passwordgenerator.database.AppDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 class Backup : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.backup_activity)
+
+
+        //db initialise
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("bob".toCharArray())//TODO change pass phrase
+        val factory = SupportFactory(passphrase)
+        val room = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-names")
+            .openHelperFactory(factory)
+            .allowMainThreadQueries()
+            .build()
+        val wordDao = room.wordDao()
+
+        //recycler initialise
+        val arr = wordDao.getAll()
+        var adapter = CustomAdapter(arr)
+        MainActivity.recyclerView = findViewById<View>(R.id.rview) as RecyclerView
+        MainActivity.recyclerView.setHasFixedSize(false)
+        MainActivity.recyclerView.setAdapter(adapter)
+        MainActivity.recyclerView.setLayoutManager(LinearLayoutManager(this))
 
     }
 }

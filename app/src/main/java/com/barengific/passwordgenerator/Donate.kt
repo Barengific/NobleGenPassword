9d70724faa88fe9650c9d800c173b69bab62101d
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-26 20:35:12 +0000

    Update Donate.kt

--- app/src/main/java/com/barengific/passwordgenerator/Donate.kt
+++ app/src/main/java/com/barengific/passwordgenerator/Donate.kt
@@ -1,12 +1,15 @@
 package com.barengific.passwordgenerator
 
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.barengific.passwordgenerator.databinding.DonationActivityBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.donation_activity.*
 import java.io.*
 
 
@@ -21,7 +24,15 @@ class Donate : AppCompatActivity() {
         binding = DonationActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        tvBTC.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvBTC.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "Text Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
 
     }
 

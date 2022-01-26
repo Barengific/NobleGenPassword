5a8ce32080ffe232cdbcdecd924f3e057529d4a6
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-26 20:41:24 +0000

    Update Donate.kt

--- app/src/main/java/com/barengific/passwordgenerator/Donate.kt
+++ app/src/main/java/com/barengific/passwordgenerator/Donate.kt
@@ -10,6 +10,8 @@ import androidx.security.crypto.MasterKey
 import com.barengific.passwordgenerator.databinding.DonationActivityBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.donation_activity.*
+import kotlinx.android.synthetic.main.donation_activity.btnCancels
+import kotlinx.android.synthetic.main.secfrag_activity.*
 import java.io.*
 
 
@@ -30,8 +32,76 @@ class Donate : AppCompatActivity() {
             // Set the clipboard's primary clip.
             clipboard.setPrimaryClip(clip)
 
-            Toast.makeText(applicationContext, "Text Copied", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "BTC Address Copied", Toast.LENGTH_SHORT).show()
             Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvETH.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvETH.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "ETH Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvBCH.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvBCH.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "BCH Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvLTC.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvLTC.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "LTC Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvETC.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvETC.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "ETC Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvAlgo.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvAlgo.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "ALGO Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        tvNano.setStartIconOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("PGen", tvNano.editText?.text.toString())
+            // Set the clipboard's primary clip.
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(applicationContext, "NANO Address Copied", Toast.LENGTH_SHORT).show()
+            Log.d("aaaaaaaaa", "copyyyingggg123")
+        }
+
+        btnCancels.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java).apply {
+                putExtra("fromDonate","cancel")
+            }
+            startActivity(intent)
+            finish()
         }
 
     }

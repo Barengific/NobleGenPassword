353ce9f24c345a2cf61ee32ec06a4e526c6dd377
Barengific <75044077+Barengific@users.noreply.github.com>
2022-01-29 18:33:01 +0000

    Update LoginResult.kt

--- app/src/main/java/com/barengific/passwordgenerator/ui/login/LoginResult.kt
+++ app/src/main/java/com/barengific/passwordgenerator/ui/login/LoginResult.kt
@@ -4,6 +4,6 @@ package com.barengific.passwordgenerator.ui.login
  * Authentication result : success (user details) or error message.
  */
 data class LoginResult(
-    val success: LoggedInUserView? = null,
+    val success: Boolean? = null,
     val error: Int? = null
 )
